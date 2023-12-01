f317c4c1c0dbdc24f21e1aaa10e5a35c5d2c115f
kevin4052 <kevin.hernandez@gmail.com>
2023-12-01 14:13:29 -0600

    day 1

--- day1/day1p2.js
+++ day1/day1p2.js
@@ -4,21 +4,21 @@ const lines = fs.readFileSync(file).toString().split('\n')
 
 const cases = {
   '0': 0,
-  '1': 2,
-  '2': 3,
-  '3': 4,
-  '4': 5,
-  '5': 6,
+  '1': 1,
+  '2': 2,
+  '3': 3,
+  '4': 4,
+  '5': 5,
   '6': 6,
   '7': 7,
   '8': 8,
   '9': 9,
   'zero': 0,
-  'one': 2,
-  'two': 3,
-  'three': 4,
-  'four': 5,
-  'five': 6,
+  'one': 1,
+  'two': 2,
+  'three': 3,
+  'four': 4,
+  'five': 5,
   'six': 6,
   'seven': 7,
   'eight': 8,
@@ -29,16 +29,29 @@ const getCalidrationValue = (lines) => {
   let sum = 0
 
   for (let i = 0; i < lines.length; i++) {
-     sum += +getFirstLastDigit(lines[i])
+     sum += getFirstLastDigit(lines[i])
   }
 
   return sum
 }
 
 const getFirstLastDigit = (line) => {
-  let numList = line.split('').filter(char => +char)
+  const keys = Object.keys(cases);
+  let arr = [];
 
-  return numList[0] + numList[numList.length - 1]
+  for (let i = 0; i < line.length; i++) {
+    const char = line[i]
+
+    for (let j = 0; j < keys.length; j++) {
+      const key = keys[j]
+      if (key[0] === char) {
+        key === line.slice(i, i + key.length) && arr.push(cases[key])
+      }
+    }
+
+  }
+
+  return arr[0] * 10 + arr[arr.length - 1]
 }
 
 console.log(getCalidrationValue(lines))
